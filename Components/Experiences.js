524c4d6aa4d2568cb9125c3f7e31ee6fc54c373f
jslopeza <jasonlopeza95@gmail.com>
2020-07-28 23:45:10 -0400

    upates

--- Components/Experiences.js
+++ Components/Experiences.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import SectionBody from './SectionBody';
+import React from "react";
+import SectionBody from "./SectionBody";
 
 const Experiences = () => (
   <React.Fragment>
@@ -8,10 +8,12 @@ const Experiences = () => (
       title="Senior Software Engineer"
       rightText="Dec 18 - Present"
       body={[
-        'Helped transition public facing application from Rails template to NextJS improving design and performance',
-        'Collaborated on moving authenticated experiences from Rails + Backbone to independant React applications improving performance and helping ship features faster',
-        'Pair programmed with collegues to learn and implement backend features in Golang or Rails as necessary',
-        'Created a single GraphQL API on Lambdas, combining multiple GraphQL and REST services providing a single layer for clients to interact with.'
+        "Contributed to the architecture and development of the Wedding Website product that lets users create and customize their personal website. This included multiple products some authenticated user facing and others public that users can share with their guests.",
+        "Helped transition public facing application from Rails templates to NextJS improving design, performance and SEO.",
+        "Created a component library for sharing features across applications, providing consistency, removing duplication of efforts and decreasing development time of new features.",
+        "Created a GraphQL API on AWS Lambdas. This GraphQL API combined multiple GraphQL and REST APIs into a single endpoint for clients to interact with.",
+        "Collaborated on replatforming authenticated application to React and Apollo client as a part of A/B test improving performance and NPS score.",
+        "Pair programmed with colleagues to learn and implement backend features in Golang and Rails as necessary.",
       ]}
     />
     <SectionBody
@@ -19,12 +21,12 @@ const Experiences = () => (
       title="Senior Application Developer"
       rightText="Apr 17 - Dec 18"
       body={[
-        `Worked on a mission critical application that provided a "One stop shop" for all service and CRM relevant information to assist clients on phone or chat.`,
-        'Created and managed NodeJS microservices for different features in the application.',
-        'Collaborated with external partners for creating REST API design.',
-        'Created REST API layer on top of legacy SOAP Web Services.',
-        'Using MongoDB, created caching layer for certain API calls to decrease response times.',
-        'Worked on modern frontend architecture, involving React, Redux and Typescript, built using Webpack',
+        `Worked on a mission critical application that provided a "One stop shop" for all. service and CRM relevant information to assist clients on phone or chat.`,
+        "Created and managed NodeJS microservices for different features in the application.",
+        "Collaborated with external partners for creating REST API design.",
+        "Created REST API layer on top of legacy SOAP Web Services.",
+        "Using MongoDB, created caching layer for certain API calls to decrease response times.",
+        "Worked on modern frontend architecture, involving React, Redux and Typescript, built using Webpack.",
       ]}
     />
     <SectionBody
@@ -32,9 +34,10 @@ const Experiences = () => (
       company="Tangential LLC"
       rightText="Apr 17 - Present"
       body={[
-        'Started consulting company to help companies build Web and Mobile applications.',
-        'Worked on an iOS application built using React Native, GraphQL and NodeJS.',
-        'Created an application to help an accounting firm to dynamically extract data from Quickbooks to Google Sheets.',
+        "Started consulting company to help companies build Web and Mobile applications.",
+        "Helped a startup with implementing yelp clone written in NodeJS and MongoDB with a React Native application.",
+        "Worked on an iOS application built using React Native, GraphQL and NodeJS.",
+        "Created an application to help an accounting firm to dynamically extract data from Quickbooks to Google Sheets.",
       ]}
     />
     <SectionBody
@@ -42,9 +45,9 @@ const Experiences = () => (
       company="gr8Dine Inc"
       rightText="Sep 15 - Dec 17"
       body={[
-        'Collaborated with clients to conduct user research and produce application requirements.',
-        'Created, lead and cultivated a team of developers to build iOS application.',
-        'Developed marketing material to showcase the application to customers and investors.',
+        "Collaborated with clients to conduct user research and produce application requirements.",
+        "Created, lead and cultivated a team of developers to build iOS application.",
+        "Developed marketing material to showcase the application to customers and investors.",
       ]}
     />
     <SectionBody
@@ -52,10 +55,10 @@ const Experiences = () => (
       company="ADP"
       rightText="Jun 16 - Dec 16"
       body={[
-        'Converted an AngularJS application to use Typescript and Webpack, modularizing our code and decreasing load times and number of requests by over 75%.',
-        'Refactored internal AngularJS library to allow compatibility with modular code.',
-        'Rewrote AngularJS application in React and Redux from ground up.',
-        'Configured development environment using Docker.',
+        "Converted an AngularJS application to use Typescript and Webpack, modularizing our code and decreasing load times and number of requests by over 75%.",
+        "Refactored internal AngularJS library to allow compatibility with modular code.",
+        "Rewrote AngularJS application in React and Redux from ground up.",
+        "Configured development environment using Docker.",
       ]}
     />
     {/* <SectionBody
